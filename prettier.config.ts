c4e42bdb2afdc50d5f8971371d62d32713202f26
Alex <alex.salerno@me.com>
2025-04-16 15:18:58 -0700

    Max line length to 120

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,7 +1,11 @@
-/** @type {import('prettier').Options} */
-module.exports = {
+import { type Options } from 'prettier'
+
+const config: Options = {
   singleQuote: true,
   semi: false,
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindStylesheet: './src/style/tailwind.css',
+  printWidth: 90
 }
+
+export default config
